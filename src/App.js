6bc088216a65869416828702e7bee194e3c0ece9
EgRoku <jimenezjj5150@gmail.com>
2023-09-16 03:33:36 -0700

    added routes and basic css rules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './containers/home';
+import Portfolio from './containers/portfolio';
+import Contact from './containers/contact';
+import Skills from './containers/skills';
+import Resume from './containers/resume';
+import About from './containers/about';
 
 function App() {
   return (
     <div className="App">
+      {/* { particles js} */}
 
+      {/* { navbar } */}
+
+      {/* { main page content } */}
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/resume' element={<Resume/>} />
+        <Route path='/skills' element={<Skills/>} />
+        <Route path='/portfolio' element={<Portfolio/>} />
+        <Route path='/contact' element={<Contact/>} />
+
+      </Routes>
     </div>
   );
 }

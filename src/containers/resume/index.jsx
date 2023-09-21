3ceb5a65fff80ae6215a9873ad2f1183ee502b51
EgRoku <jimenezjj5150@gmail.com>
2023-09-21 16:26:24 -0700

    added headerstyling for pages and created the section for each page

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { BsInfoCircleFill } from 'react-icons/bs';
+import PageHeaderContent from "../../components/pageHeaderContent";
 
 const Resume = () => {
     return (
-        <div>
-            Resume Page
-        </div>
+        <section id='resume' className="resume">
+
+            <PageHeaderContent
+            headerText = 'Resume'
+            icon = { <BsInfoCircleFill size={40} />}
+            />
+
+        </section>
     )
 }
 

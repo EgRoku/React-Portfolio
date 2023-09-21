64306b825f2afb2d8d0060840469ec14722b4fe6
EgRoku <jimenezjj5150@gmail.com>
2023-09-21 14:25:48 -0700

    finally got particles to work properly using options file in utils/particles.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 import Home from './containers/home';
 import Portfolio from './containers/portfolio';
 import Contact from './containers/contact';
@@ -7,11 +9,19 @@ import Skills from './containers/skills';
 import Resume from './containers/resume';
 import About from './containers/about';
 import Navbar from './components/navBar';
+import particles from './utils/particles';
 
 function App() {
+
+  const handleInit = async (main) => {
+    await loadFull(main)
+  }
+
   return (
     <div className="App">
       {/* { particles js} */}
+
+      <Particles id='particles' options={particles} init={handleInit}/>
 
       {/* { navbar } */}
       <Navbar/>

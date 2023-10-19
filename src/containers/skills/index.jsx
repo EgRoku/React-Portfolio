edbe4a96842dce724c68206929e52bcc92f77b7a
EgRoku <jimenezjj5150@gmail.com>
2023-10-18 17:09:47 -0700

    added skills section as well as its styling

--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,16 +1,67 @@
 import React from "react";
 import { BsInfoCircleFill } from 'react-icons/bs';
 import PageHeaderContent from "../../components/pageHeaderContent";
+import { skillsData } from "./utils";
+import { Animate } from "react-simple-animate";
+import { Line } from "rc-progress";
+import './styles.scss';
+
 
 const Skills = () => {
     return (
         <section id='skills' className="skills">
 
             <PageHeaderContent
-            headerText = 'My Skills'
-            icon = { <BsInfoCircleFill size={40} />}
+                headerText='My Skills'
+                icon={<BsInfoCircleFill size={40} />}
             />
-
+            <div className="skillsContentWrapper">
+                {
+                    skillsData.map((item, i) => (
+                        <div key={i} className="skillsContentWrapperInnerContent">
+                            <Animate
+                                play
+                                duration={1}
+                                delay={0.3}
+                                start={{
+                                    transform: 'translateX(-200px)'
+                                }}
+                                end={{
+                                    transform: 'translateX(0px)'
+                                }}
+                            >
+                                <h3 className="skillsContentWrapperInnerContentCategoryText">{item.label}</h3>
+                                <div>
+                                    {
+                                        item.data.map((skillItem, j) => (
+                                            <Animate
+                                                play
+                                                duration={1}
+                                                keyFrames={['opacity : 1', 'opacity : 0']}
+                                                iterationCount='1'
+                                            >
+                                                <div className="progressbarWrapper" key={j}>
+                                                    <p>
+                                                        {skillItem.skillName}
+                                                    </p>
+                                                    <Line
+                                                        percent={skillItem.percentage}
+                                                        strokeWidth='2'
+                                                        strokeColor='var(--cyan-theme-main-color)'
+                                                        trailWidth='2'
+                                                        strokeLinecap="square"
+                                                    >
+                                                    </Line>
+                                                </div>
+                                            </Animate>
+                                        ))
+                                    }
+                                </div>
+                            </Animate>
+                        </div>
+                    ))
+                }
+            </div>
         </section>
     )
 }
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,61 @@
+export const skillsData = [
+    {
+        label: "Front End",
+        data: [
+            {
+                skillName: "HTML",
+                percentage: '90',
+            },
+            {
+                skillName: 'CSS',
+                percentage: '90',
+            },
+            {
+                skillName: 'JavaScript',
+                percentage: '80',
+            },
+            {
+                skillName: 'React JS',
+                percentage: '80',
+            },
+            {
+                skillName: 'SCSS',
+                percentage: '70',
+            },
+        ],
+    },
+    {
+        label: 'Back End',
+        data: [
+            {
+                skillName: 'Express',
+                percentage: '80',
+            },
+            {
+                skillName: 'JavaScript',
+                percentage: '80',
+            },
+            {
+                skillName: 'Node.Js',
+                percentage: '90',
+            },
+            {
+                skillName: 'Handlebars',
+                percentage: '70',
+            },
+        ],
+    },
+    {
+        label: 'Databases',
+        data: [
+            {
+                skillName: 'MYSQL',
+                percentage: '80',
+            },
+            {
+                skillName: 'MongoDb',
+                percentage: '70',
+            },
+        ],
+    },
+];
